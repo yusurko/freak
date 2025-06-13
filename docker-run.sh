e5e15c6b839f3b1eb1b1a5b26f453ec84b7ae2c3
Mattia Succurro <sakuragasaki46@gmail.com>
2025-06-13 15:54:06 +0200

    fix Docker build

--- docker-run.sh
+++ docker-run.sh
@@ -3,7 +3,8 @@
 start-app() {
     [[ ! -d /opt/live-app ]] && exit 1
     cd /usr/src/app
-    cp -rv /opt/live-app/{freak,pyproject.toml,.env,docker-run.sh} ./
+    cp -rv /opt/live-app/{freak,pyproject.toml,docker-run.sh} ./
+    cp -v /opt/live-app/.env.prod .env
     pip install -e .
     flask --app freak run --host=0.0.0.0 
 }
